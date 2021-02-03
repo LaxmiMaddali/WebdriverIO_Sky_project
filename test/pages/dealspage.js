6ba11cf8dc8fc42b525a3507061686fddce42886
LaxmiMaddali <laxmi.maddali20@gmail.com>
2021-02-03 14:54:47 +0000

    Update dealspage.js

--- test/pages/dealspage.js
+++ test/pages/dealspage.js
@@ -12,6 +12,32 @@ class DealsPage extends Page {
     acceptCookie(){
         super.acceptCookie();
     }
+
+    waitForElement(element){
+        super.waitForElement(element)
+    }
+
+getListofOfferPrices(){
+    for(var i=2; i<4; i++){
+        it('',function(){
+           let elements= $$('#tab-1')[0]
+           var childElem = elements.$$('div')[i].$('span[class="c-price__main"]')
+           
+           childElem.scrollIntoView()
+           console.log(childElem.getText())
+
+           if(i=2){
+               expect(childElem).toHaveTextContaining('39')
+           } else if(i=3){
+               expect(childElem).toHaveTextContaining('47')
+           } else if(i=4){
+               expect(childElem).toHaveTextContaining('25')
+                }
+         })
+       }
+}
+
+
 }
 
 module.exports = new DealsPage();
