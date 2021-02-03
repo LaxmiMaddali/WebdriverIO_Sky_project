8b31dfddf12a194dacf1f64c0194c5af86c33d30
LaxmiMaddali <laxmi.maddali20@gmail.com>
2021-02-03 14:56:10 +0000

    Update dealstest.js

--- test/specs/dealstest.js
+++ test/specs/dealstest.js
@@ -8,46 +8,13 @@ describe('validating prices on deals page', () => {
     })
 
     it('Clicking and validating deals page', () => {
-        browser.setTimeout({ 'implicit': 50000 })
+        waitForElement(DealsPage.deals);
         DealsPage.deals.click()
-        browser.setTimeout({ 'implicit': 50000 })
         expect(browser).toHaveUrl('https://www.sky.com/shop/offers')
     })
 
     it('validating the offers on deals page',()=>{
-       
-        for(var i=2; i<4; i++){
-         it('',function(){
-            let elements= $$('#tab-1')[0]
-            var childElem = elements.$$('div')[i].$('span[class="c-price__main"]')
-            
-            console.log("this is the value --------- ")
-            childElem.scrollIntoView()
-            console.log(childElem.getText())
-
-            if(i=2){
-                expect(childElem).toHaveTextContaining('39')
-            } else if(i=3){
-                expect(childElem).toHaveTextContaining('47')
-            } else if(i=4){
-                expect(childElem).toHaveTextContaining('25')
-                 }
-          })
-        }
+       DealsPage.getListofOfferPrices();
+        
     })
 })
-
-
-/*
-
-  var firstOffer = $('#TP42 span[class="c-price__main"]')
-         expect(firstOffer).toHaveTextContaining('39')
-
-         var secondOffer = $('#TPUF span[class="c-price__main"]')
-         expect(secondOffer).toHaveTextContaining('47')
-
-         var thirdOffer = $('#sabb span[class="c-price__main"]')
-         expect(thirdOffer).toHaveTextContaining('25')
-
-
-        }*/
