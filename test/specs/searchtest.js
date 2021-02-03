56b17027e3bf563a171472048f36e9a887147d79
LaxmiMaddali <laxmi.maddali20@gmail.com>
2021-02-03 14:56:43 +0000

    Update searchtest.js

--- test/specs/searchtest.js
+++ test/specs/searchtest.js
@@ -8,17 +8,16 @@ describe ('validating the editorial section via search bar', ()=>{
     })
 
 it('click on search button on homepage',()=>{
-    browser.setTimeout({implicit : 10000})
     SearchPage.searchicon.click();
 })
 
 it('enter value into the search field', ()=>{
        SearchPage.enterValueInSearchField.addValue('Sky');
        SearchPage.clickOnSearchIcon.click();
-       browser.setTimeout({implicit : 10000})
+       
 })
  it('Validating the editorial section on Sky page',()=>{
-     browser.setTimeout({implicit : 10000})
+     waitForElement(SearchPage.editorialSection)
      expect(SearchPage.editorialSection).toBeVisible();
  })
 
