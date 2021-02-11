2a0b79e75e3bac0b2681b136daba0c9b529115e3
LaxmiMaddali <laxmi.maddali20@gmail.com>
2021-02-11 16:28:00 +0000

    Update signintest.js

--- test/specs/signintest.js
+++ test/specs/signintest.js
@@ -10,7 +10,7 @@ describe('Sign In Validation', () => {
     it('Clicking on signin link on homepaege', () => {
         browser.setTimeout({ 'implicit': 10000 })
         SigninPage.signInLink.click()
-        signinpage.signInPageTitle.waitForDisplayed({timeout : 3000})
+        SigninPage.signInPageTitle.waitForDisplayed({timeout : 3000})
         expect(browser).toHaveTitle('Sky - Sign in')
     })
 
@@ -21,18 +21,15 @@ describe('Sign In Validation', () => {
 
     it('Entering the password ', ()=> {
         SigninPage.password.setValue('maddali');
-        browser.setTimeout({ 'implicit': 10000 })
     })
 
     it('click on sign-in',()=>{
         SigninPage.signInButton.click();
-        browser.setTimeout({ 'implicit': 10000 })
     })
 
     it('validating the error message',()=>{
         const errorMessage = $('#signInPanel ul > li')
         browser.setTimeout({ 'implicit': 10000 })
-
         expect(errorMessage.getText()).toHaveTextContaining('Sorry, we did not recognise either your username or password')
     })
 })
